d45375a48f886ea23073266ec64ed21c4e6d070f
kosiarah <kosiarah@gmail.com>
2024-06-13 14:05:51 +0100

    Create dictionary_assignmet.py
    This code creates a dictionary with numbers 1 - 10, then loops through those numbers and multiplies by 2 on each iteration.

---
+++ dictionary_assignmet.py
@@ -0,0 +1,16 @@
+numbers_dict = {
+  "one" : 1,
+  "two" : 2,
+  "three" : 3,
+  "four" : 4,
+  "five" : 5,
+  "six" : 6,
+  "seven" : 7,
+  "eight" : 8,
+  "nine" : 9,
+  "ten" : 10
+}
+
+for number in numbers_dict
+  if number in numbers_dict
+    print(numbers_dict[number] * 2)
